7d7ff63e6289d74e086ee185b1a351080db3dbd4
unknown <sjdnez9402@gmail.com>
2024-12-22 17:14:49 +0330

    install drf_spectacular

--- app/settings.py
+++ app/settings.py
@@ -37,11 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     "rest_framework",
     "corsheaders",
+    'drf_spectacular',
     
     "core",
 ]
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -147,3 +154,12 @@ EMAIL_PORT = 1025
 EMAIL_USE_TLS = False  
 EMAIL_USE_SSL = False  
 DEFAULT_FROM_EMAIL = 'from@example.com'
+
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Auth API',
+    'DESCRIPTION': 'Auth Application',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import *
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 #--------------------------------
 
 #/api/""
@@ -14,4 +14,9 @@ urlpatterns = [
     path("logout", LogoutApiview.as_view(), name=""),
     path("forgot", forgotApiView.as_view(), name=""),
     path("reset", ResetApiView.as_view(), name=""),
+    
+    #swagger
+    path('schema', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
